6972a3a19ce3b301fb7274d21e09a9ea509114f8
Vu Doan <vudoancs@gmail.com>
2025-07-18 21:01:35 +0700

    fix

--- src/config/dev.config.ts
+++ src/config/dev.config.ts
@@ -1,3 +1,5 @@
 export const configuration = {
-  PORT: 8001,
+  PORT: 6001,
+  MONGO_URL:"mongodb+srv://sm-user-dev:DgxrKWf74KChFgS6@cluster0.ma8csfb.mongodb.net/sm-dev",
+  GOOGLE_CLIENT_ID:"155405497118-gsd82i7ge0rl49jmpjrupie25mcka082.apps.googleusercontent.com"
 };
---
+++ src/config/dist/dev.config.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.configuration = void 0;
+exports.configuration = {
+    PORT: 6001,
+    MONGO_URL: "mongodb+srv://sm-user-dev:DgxrKWf74KChFgS6@cluster0.ma8csfb.mongodb.net/sm-dev",
+    GOOGLE_CLIENT_ID: "155405497118-gsd82i7ge0rl49jmpjrupie25mcka082.apps.googleusercontent.com"
+};
