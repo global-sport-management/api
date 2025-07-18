aa5fba0479f456917055ec028cc49b1b50e4f4ae
Vu Doan <vudoancs@gmail.com>
2025-07-18 21:30:14 +0700

    fix

--- src/config/default.config.ts
+++ src/config/default.config.ts
@@ -2,4 +2,6 @@ export const defaultConfig = {
   PORT: 6001,
   JWT_SECRET: '94rEvCERhR',
   JWT_EXPIRES_IN:'30d',
+  MONGO_URL:"mongodb+srv://sm-user-dev:DgxrKWf74KChFgS6@cluster0.ma8csfb.mongodb.net/sm-dev",
+  GOOGLE_CLIENT_ID:"155405497118-gsd82i7ge0rl49jmpjrupie25mcka082.apps.googleusercontent.com"
 };
--- src/config/dist/dev.config.js
+++ src/config/dist/dev.config.js
@@ -2,7 +2,5 @@
 exports.__esModule = true;
 exports.configuration = void 0;
 exports.configuration = {
-    PORT: 6001,
-    MONGO_URL: "mongodb+srv://sm-user-dev:DgxrKWf74KChFgS6@cluster0.ma8csfb.mongodb.net/sm-dev",
-    GOOGLE_CLIENT_ID: "155405497118-gsd82i7ge0rl49jmpjrupie25mcka082.apps.googleusercontent.com"
+    PORT: 6001
 };
